71a8ae735292e40f74bd30c04e5440760b4b5084
ism <you@example.com>
2022-02-27 16:28:36 +0000

    initial - HEAVILY based on chris@machine

---
+++ lua/user/colorscheme.lua
@@ -0,0 +1,11 @@
+-- vim.cmd "colorscheme onedarker"
+
+local colorscheme = "tokyonight"
+vim.g.tokyonight_style = "night"
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+	vim.notify("colorscheme " .. " not found!")
+	return
+end
+
