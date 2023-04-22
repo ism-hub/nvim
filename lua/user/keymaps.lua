0852d9bbcddca7599c525991f8741439cefa9b51
ism <you@example.com>
2023-04-22 09:37:26 +0000

    keymaps: "gr" changing lsp show references to use telescope's view, also added show marks(<leader>fm), find-in-buffer(<leader>f/) and show-references(<leader>fr)

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -48,6 +48,9 @@ vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+vim.keymap.set("n", "<leader>f/", builtin.current_buffer_fuzzy_find, {})
+vim.keymap.set("n", "<leader>fm", builtin.marks, {})
+vim.keymap.set("n", "<leader>fr", builtin.lsp_references, {})
 
 -- easy save
 keymap("n", "<leader>w", ":w<CR>", opts)
