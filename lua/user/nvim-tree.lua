5ab1e9e29473864d3db34e2ecb94b52228408fd5
ism <you@example.com>
2023-04-22 12:47:19 +0000

    keymaps: changing nvim-tree and telescope to open window vertically using <C-y> keymap (<C-x> is for horizontally and left the same)

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -1,1 +1,14 @@
-require("nvim-tree").setup({})
+-- from docs
+local function my_on_attach(bufnr)
+	local api = require("nvim-tree.api")
+	local function opts(desc)
+		return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+	end
+	api.config.mappings.default_on_attach(bufnr)
+	-- your removals and mappings go here
+
+	vim.keymap.set("n", "<C-y>", api.node.open.vertical, opts("Open: Vertical Split"))
+	vim.keymap.set("n", "<C-x>", api.node.open.horizontal, opts("Open: Horizontal Split"))
+end
+
+require("nvim-tree").setup({ on_attach = my_on_attach })
--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -27,7 +27,7 @@ telescope.setup {
 
         ["<CR>"] = actions.select_default,
         ["<C-x>"] = actions.select_horizontal,
-        ["<C-v>"] = actions.select_vertical,
+        ["<C-y>"] = actions.select_vertical,
         ["<C-t>"] = actions.select_tab,
 
         ["<C-u>"] = actions.preview_scrolling_up,
