fd7d4b96ca53f28f0a9c5178055793089f69025e
ism <you@example.com>
2023-04-22 08:12:11 +0000

    keymaps: changing telescope keymaps (more clean) and adding keymaps to search open buffers.

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -43,14 +43,11 @@ keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
 keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
 
 -- Telescope
-keymap(
-	"n",
-	"<leader>f",
-	"<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false, hidden=true, no_ignore=true }))<cr>",
-	opts
-)
---[[ keymap("n", "<leader>g", "<cmd>Telescope live_grep<cr>", opts) ]]
-keymap("n", "<leader>g", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", opts)
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 
 -- easy save
 keymap("n", "<leader>w", ":w<CR>", opts)
