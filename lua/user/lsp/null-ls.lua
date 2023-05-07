bd1319636f78bee194391ed2a50eb4704c08b197
ism <you@example.com>
2023-05-07 19:31:21 +0000

    change: making the nvim cspell conf the default one for all and saving it in git

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -6,37 +6,37 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
-    debug = false,
-    sources = {
-        null_ls.builtins.diagnostics.cspell.with({
-            filetypes = { "lua", "rust" },
-            -- Force the severity to be HINT
-            diagnostics_postprocess = function(diagnostic)
-                diagnostic.severity = vim.diagnostic.severity.HINT
-            end,
-        }),
-        null_ls.builtins.code_actions.cspell.with({
-            config = {
-                --[[ find_json = function(cwd) ]]
-                --[[     return vim.fn.expand(cwd .. "/cspell.json") ]]
-                --[[ end, ]]
-                --[[ on_success = function(cspell_config_file, params) ]]
-                --[[     -- format the cspell config file ]]
-                --[[     os.execute( ]]
-                --[[         string.format( ]]
-                --[[             "cat %s | jq -S '.words |= sort' | tee %s > /dev/null", ]]
-                --[[             cspell_config_file, ]]
-                --[[             cspell_config_file ]]
-                --[[         ) ]]
-                --[[     ) ]]
-                --[[ end, ]]
-            },
-            filetypes = { "lua", "rust" },
-        }),
-        formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-        formatting.black.with({ extra_args = { "--fast" } }),
-        -- formatting.yapf,
-        formatting.stylua,
-        --[[ diagnostics.flake8, ]]
-    },
+	debug = false,
+	sources = {
+		null_ls.builtins.diagnostics.cspell.with({
+			filetypes = { "lua", "rust" },
+			-- Force the severity to be HINT
+			diagnostics_postprocess = function(diagnostic)
+				diagnostic.severity = vim.diagnostic.severity.HINT
+			end,
+		}),
+		null_ls.builtins.code_actions.cspell.with({
+			config = {
+				find_json = function(cwd)
+					return vim.fn.expand("~/.config/nvim/cspell.json")
+				end,
+				--[[ on_success = function(cspell_config_file, params) ]]
+				--[[ 	-- format the cspell config file ]]
+				--[[ 	os.execute( ]]
+				--[[ 		string.format( ]]
+				--[[ 			"cat %s | jq -S '.words |= sort' | tee %s > /dev/null", ]]
+				--[[ 			cspell_config_file, ]]
+				--[[ 			cspell_config_file ]]
+				--[[ 		) ]]
+				--[[ 	) ]]
+				--[[ end, ]]
+			},
+			filetypes = { "lua", "rust" },
+		}),
+		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+		formatting.black.with({ extra_args = { "--fast" } }),
+		-- formatting.yapf,
+		formatting.stylua,
+		--[[ diagnostics.flake8, ]]
+	},
 })
