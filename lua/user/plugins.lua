6cc73e7145fde40337f4d7c90a23cf3650718995
ism <you@example.com>
2023-05-09 07:27:00 +0000

    fixes: not lazy loading lspsaga (for toggle term) and fixing search history keymap

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -110,10 +110,10 @@ require("lazy").setup({
 	-- nice lsp ui (ui for code actions and more)
 	{
 		"glepnir/lspsaga.nvim",
-		event = "LspAttach",
-		config = function()
-			require("lspsaga").setup({})
-		end,
+		--[[ event = "LspAttach", ]]
+		--[[ config = function() ]]
+		--[[ 	require("lspsaga").setup({}) ]]
+		--[[ end, ]]
 		dependencies = {
 			{ "nvim-tree/nvim-web-devicons" },
 			--Please make sure you install markdown and markdown_inline parser
