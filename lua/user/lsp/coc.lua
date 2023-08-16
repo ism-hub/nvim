0fa701ad35867de0fdbb4576a30b65a636389214
ism <you@example.com>
2023-08-16 14:43:10 +0000

    add: coc-symbol-line

--- lua/user/lsp/coc.lua
+++ lua/user/lsp/coc.lua
@@ -14,3 +14,15 @@ vim.opt.writebackup = false
 -- Having longer updatetime (default is 4000 ms = 4s) leads to noticeable
 -- delays and poor user experience
 vim.opt.updatetime = 300
+
+-- symbol-line
+-- :CocInstall coc-symbol-line
+function _G.symbol_line()
+    local bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid or 0)
+    local ok, line = pcall(vim.api.nvim_buf_get_var, bufnr, 'coc_symbol_line')
+    return ok and '%#CocSymbolLine# ' .. line or ''
+end
+
+-- vim.o.tabline = '%!v:lua.symbol_line()'
+-- vim.o.statusline = '%!v:lua.symbol_line()'
+vim.o.winbar = '%!v:lua.symbol_line()'
