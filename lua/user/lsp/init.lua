012c1fcc5de7ad0a13e1315df6838e23cd71998d
ism <you@example.com>
2024-01-08 11:36:23 +0200

    change: ensuring lua_ls, clangd and rust-analyzer lsps are installed

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -3,7 +3,9 @@ require("mason").setup()
 require("mason-nvim-dap").setup({
     ensure_installed = { "codelldb", "cpptools" } -- for rust
 })
-require("mason-lspconfig").setup()
+require("mason-lspconfig").setup {
+    ensure_installed = {"lua_ls", "clangd", "rust_analyzer" },
+}
 require("user.lsp.server_settings")
 require("user.lsp.cmp")
 
