7c77febec70b68c8318b00cd23fc4748da320106
ism <you@example.com>
2024-06-28 11:51:58 +0300

    Add: signature help (for overloads)

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -35,6 +35,7 @@ require("lazy").setup({
         'hrsh7th/cmp-path',
         'hrsh7th/cmp-cmdline',
         'hrsh7th/nvim-cmp',
+        'hrsh7th/cmp-nvim-lsp-signature-help',
         -- snips
         'hrsh7th/cmp-vsnip',
         'hrsh7th/vim-vsnip',
