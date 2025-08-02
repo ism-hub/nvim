f0d9c62978495c9a043d84ecf7ea156b44ca0712
ism <you@example.com>
2025-08-02 12:56:45 +0300

    add: c# lsp configuration

--- lua/user/lsp/server_settings.lua
+++ lua/user/lsp/server_settings.lua
@@ -65,6 +65,7 @@ require("mason-lspconfig").setup_handlers({
     ["omnisharp"] = function()
         require("lspconfig")["omnisharp"].setup({
             capabilities = capabilities,
+            flags = { debounce_text_changes = 800, },
             handlers = {
                 ["textDocument/definition"] = require('omnisharp_extended').definition_handler,
                 ["textDocument/typeDefinition"] = require('omnisharp_extended').type_definition_handler,
@@ -72,6 +73,45 @@ require("mason-lspconfig").setup_handlers({
                 ["textDocument/implementation"] = require('omnisharp_extended').implementation_handler,
             },
             root_dir = util.root_pattern("*.sln", "**/*.sln"),
+            settings = {
+                FormattingOptions = {
+                    -- Enables support for reading code style, naming convention and analyzer
+                    -- settings from .editorconfig.
+                    EnableEditorConfigSupport = true,
+                    -- Specifies whether 'using' directives should be grouped and sorted during
+                    -- document formatting.
+                    OrganizeImports = false,
+                },
+                MsBuild = {
+                    -- If true, MSBuild project system will only load projects for files that
+                    -- were opened in the editor. This setting is useful for big C# codebases
+                    -- and allows for faster initialization of code navigation features only
+                    -- for projects that are relevant to code that is being edited. With this
+                    -- setting enabled OmniSharp may load fewer projects and may thus display
+                    -- incomplete reference lists for symbols.
+                    LoadProjectsOnDemand = false,
+                },
+                RoslynExtensionsOptions = {
+                    -- Enables support for roslyn analyzers, code fixes and rulesets.
+                    EnableAnalyzersSupport = false,
+                    -- Enables support for showing unimported types and unimported extension
+                    -- methods in completion lists. When committed, the appropriate using
+                    -- directive will be added at the top of the current file. This option can
+                    -- have a negative impact on initial completion responsiveness,
+                    -- particularly for the first few completion sessions after opening a
+                    -- solution.
+                    EnableImportCompletion = false,
+                    -- Only run analyzers against open files when 'enableRoslynAnalyzers' is
+                    -- true
+                    AnalyzeOpenDocumentsOnly = false,
+                    EnableDecompilationSupport = true,
+                },
+            },
+            Sdk = {
+                -- Specifies whether to include preview versions of the .NET SDK when
+                -- determining which version to use for project loading.
+                IncludePrereleases = true,
+            },
         })
     end,
     ["gopls"] = function()
