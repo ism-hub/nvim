05e9b21d0c582eb69aa02977a425da4085807fc9
ism <you@example.com>
2025-08-03 17:38:57 +0300

    fix: correct cwd when opening the main terminal

--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -10,7 +10,15 @@ require("toggleterm").setup({
 })
 
 -- term 1 is always the floating one
-local Terminal      = require('toggleterm.terminal').Terminal
-local floating_term = Terminal:new({ direction = 'float', count = 1 })
+local Terminal = require('toggleterm.terminal').Terminal
+local floating_term = Terminal:new({ dir = vim.fn.getcwd(), direction = 'float', count = 1 })
 floating_term:spawn()
 vim.cmd [[ :1ToggleTermSetName terminal ]]
+
+-- when we start nvim with some path (i.e `nvim <dir>`) we call this change dir event
+vim.api.nvim_create_autocmd("DirChanged", {
+    -- once = true,
+    callback = function()
+        floating_term:change_dir(vim.fn.getcwd())
+    end,
+})
